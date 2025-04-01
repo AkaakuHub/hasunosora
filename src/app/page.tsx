8bd1c6868a59e75d298721ea347c579b0bba32fd
AkaakuHub <Akaaku2021media01@gmail.com>
2025-04-01 18:23:07 +0900

    fix: 全体スタイル

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -220,7 +220,7 @@ export default function Home() {
 						</div>
 					</div>
 
-					<div className="order-1 lg:order-2 lg:col-span-3 flex flex-col gap-6">
+					<div className="order-1 lg:order-2 lg:col-span-3 flex flex-col gap-20 md:gap-6 px-8 md:px-0">
 						<Section title="ニュース" onClick={() => handleModal("normal")}>
 							<News {...AprilfoolProps} />
 						</Section>
--- src/components/(pages)/about/index.tsx
+++ src/components/(pages)/about/index.tsx
@@ -2,17 +2,17 @@ import type React from "react";
 
 const About: React.FC = () => {
 	return (
-		<div className="p-4 rounded-b-lg mb-48">
+		<div className="p-4 rounded-b-lg mb-24 flex flex-col gap-4 mx-auto max-w-[1000px]">
 			<div className="flex flex-col items-start gap-2.5 md:mt-8">
-				<p className="font-['Yuji_Syuku'] font-normal inline-block bg-gradient-to-br from-white/60 to-transparent bg-no-repeat bg-[length:100%_2em] bg-[position:0_100%] transition-[background-size] duration-300 rounded-[10px] opacity-0 animate-fade-in text-[3em] md:text-[2.1em] my-4 max-w-[80%]">
+				<p className="font-['Yuji_Syuku'] font-normal inline-block bg-gradient-to-br from-white/60 to-transparent bg-no-repeat bg-[length:100%_2em] bg-[position:0_100%] transition-[background-size] duration-300 rounded-[10px] opacity-0 animate-fade-in text-[2.4em] md:text-[2.1em] my-4 max-w-[80%]">
 					蓮の花咲く湖の傍に建つ
 				</p>
-				<p className="font-['Yuji_Syuku'] font-normal inline-block bg-gradient-to-br from-white/60 to-transparent bg-no-repeat bg-[length:100%_2em] bg-[position:0_100%] transition-[background-size] duration-300 rounded-[10px] opacity-0 animate-fade-in-delay-1 text-[3em] md:text-[2.1em] my-4 max-w-[80%] self-end">
+				<p className="font-['Yuji_Syuku'] font-normal inline-block bg-gradient-to-br from-white/60 to-transparent bg-no-repeat bg-[length:100%_2em] bg-[position:0_100%] transition-[background-size] duration-300 rounded-[10px] opacity-0 animate-fade-in-delay-1 text-[2.4em] md:text-[2.1em] my-4 max-w-[80%] self-end">
 					創立から100年以上の伝統が続く
 				</p>
 			</div>
 			<div className="text-center">
-				<p className="font-['Yuji_Syuku'] font-normal inline-block bg-gradient-to-br from-white/60 to-transparent bg-no-repeat bg-[length:100%_2em] bg-[position:0_100%] transition-[background-size] duration-300 rounded-[10px] opacity-0 animate-fade-in-delay-2 text-[3em] md:text-[2.1em] my-4 max-w-full">
+				<p className="font-['Yuji_Syuku'] font-normal inline-block bg-gradient-to-br from-white/60 to-transparent bg-no-repeat bg-[length:100%_2em] bg-[position:0_100%] transition-[background-size] duration-300 rounded-[10px] opacity-0 animate-fade-in-delay-2 text-[2.4em] md:text-[2.1em] my-4 max-w-full">
 					私立蓮ノ空女学院
 				</p>
 			</div>
